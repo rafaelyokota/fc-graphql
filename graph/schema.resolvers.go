4a4f6dd07c54ad5777510863ed0b9485be2796cc
rafael Yokota <rafael.yokota.jp@gmail.com>
2022-04-24 09:24:34 -0300

    gerado esqueleto da aplicação  baseado no schema disponivel

---
+++ graph/schema.resolvers.go
@@ -0,0 +1,58 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/rafaelyokota/fc-graphql/graph/generated"
+	"github.com/rafaelyokota/fc-graphql/graph/model"
+)
+
+func (r *mutationResolver) CreateCategory(ctx context.Context, input *model.NewCategory) (*model.Category, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) CreateCourse(ctx context.Context, input *model.NewCourse) (*model.Course, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) CreateChapter(ctx context.Context, input *model.NewChapter) (*model.Chapter, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Courses(ctx context.Context) ([]*model.Course, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Chapters(ctx context.Context) ([]*model.Chapter, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//    it when you're done.
+//  - You have helper methods in this file. Move them out to keep these resolver files clean.
+// func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+// 	panic(fmt.Errorf("not implemented"))
+// }
+// func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+// 	panic(fmt.Errorf("not implemented"))
+// }
--- readme.md
+++ readme.md
@@ -20,11 +20,27 @@
 
     - go mod init github.com/rafaelyokota/fc-graphql
     - go get github.com/99designs/gqlgen
-    - go run github.com/99designs/gqlgen init
-    - deletar arquivos gerados depois do comando init 
-    - criar pasta "graph"
-    - criado schema.graphql
+    - go run github.com/99designs/gqlgen init //cria modelo de exemplo
+    - alterar  schema.graphql 
+    - no arquivo graph/schema.resolvers.go
+        no final do arquivo deletar ou comentar linhas que conter os "metodos":
+            - CreateTodo
+            - Todos
 
+    - go run github.com/99designs/gqlgen generate
+
+## Notas
+    - Arquivo de resolver.go 
+        - EX: resolver.go para categoria requisitada pelo client, caso precise buscar dados para complementar a categoria, em uma outra api, servico, banco, depois retornar ao que cliente requisitou (insert ou update ou delete)
+        - resolver.go é o cara que faz injeção de depencia
+        - A regra de negocio pode ficar nesse arquivo
+
+    - No arquivo de 'schema.resolvers.go'
+        - é como se fosse "controller" de uma API, não tem "regra de negocio" nesse arquivo
+        - utilizar resolver.go (injeção de dependencias)
+        - é o arquivo onde você implementa o "select"(ou utilizando repostory)  por exemplo
+        - tem types de "mutation" e "query" que faze gerenciamento do Resolver
+    
 # Referencias 
 
     - https://code.visualstudio.com/docs/remote/create-dev-container
